bfcd9f29298f72799c48d11fce1a66c1cb594b4e
Mustafa Brooky <97129331+MustafaBR213@users.noreply.github.com>
2023-03-23 03:04:53 +0300

    Add files via upload

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin'
+        ]);
+    }
+}
